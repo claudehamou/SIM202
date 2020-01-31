075d493f249ea37a20f14dc64cf5d80245ee61e9
irinaandria <60254169+irinaandria@users.noreply.github.com>
2020-01-31 11:15:40 +0100

    Classe individu OK

--- algoGenetique.h
+++ algoGenetique.h
@@ -7,24 +7,7 @@ using namespace std;
 class Individu
 {
 public :
-    int tailleGenome;
-    int * genome;
-
-    void init(int n) {tailleGenome = n; genome = new int[n];}; //allocation
-    void clearing() {delete genome;}; //desallocation
-
-    // constructeurs / destructeur
-
-    Individu(int n) {init(tailleGenome);}; //constructeur par defaut
-    Individu(const Individu& ind); //constructeur par copie
-    ~Individu();
-
-    // surcharges operateurs
-
-    double operator()(int i) const {return genome[i-1];}; // ieme coordonnee en lecteur
-    double& operator()(int i) {return genome[i-1];}; // ieme coordonnee en ecriture
-
-    virtual double & adaptation()=0; //fonction d'adaptation virtuelle pure a ecrire concretement dans la classe chemin
+    virtual double  adaptation()=0; //fonction d'adaptation virtuelle pure a ecrire concretement dans la classe chemin
 };
 
 class Population
