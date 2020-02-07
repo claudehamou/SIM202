8791c50fad9be37b6ad5986ec9e6eb8398f1a17e
irinaandria <60254169+irinaandria@users.noreply.github.com>
2020-02-07 11:15:54 +0100

    ~Chemin non nécessaire Luné

--- graphe_chemin.hpp
+++ graphe_chemin.hpp
@@ -48,7 +48,6 @@ public :
     Chemin();
     Chemin(Graphe*);    //construction d'un chemin a partir d'un graphe
     Chemin & operator = (const Chemin &);       //constructeur par copie
-    ~Chemin();
 
     int operator()(int i) const {return villes.at(i);}; // ieme coordonnee en lecteur
     int& operator()(int i) {return villes.at(i);}; // ieme coordonnee en ecriture
