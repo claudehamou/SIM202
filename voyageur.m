537d970f7458674e41075e5f2e2e182981347cb7
irinaandria <60254169+irinaandria@users.noreply.github.com>
2020-03-26 19:20:14 +0100

    Partie 1 affichage matlab

--- voyageur.m
+++ voyageur.m
@@ -49,10 +49,3 @@ for k=1:nbr_villes
 end 
 fclose(fid) ;
 
-% Lire un fichier pour le chemin optimal
-% Format du fichier un espace entre chaque ville
-f_chemin = fopen('Chemin.txt','r') ;
-chemin = fscanf(f_chemin,'%d',nbr_villes*2) ;
-chemin = chemin' ;
-  
-affiche_chemin(chemin,Image,x,y,nbr_villes) ;
