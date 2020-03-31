40d3fc810679a55417ae8486a5fc6b16862dab12
eliseharivel <60254184+eliseharivel@users.noreply.github.com>
2020-03-31 16:21:26 +0200

    commentaires

--- main.cpp
+++ main.cpp
@@ -9,78 +9,86 @@ int main()
 {
     //////////////////// LECTURE D'UN FICHIER EDITE SUR MATLAB POUR INITIALISATION DU GRAPHE ///////////////
 
-    ifstream mon_graphe("E:/ENSTA/2A/SIM/202/Matrice_distance.txt",ios::in) ; // Ouverture mode lecture
+    ifstream mon_graphe("C:/.../Matrice_distance.txt", ios::in); // A modifier /Ouverture mode lecture
 
-    string ligne ;
-    double distance ;
-    int nbr ;
-    int i=0 ;
+    string ligne;
+    double distance;
+    int nbr;
+    int i = 0;
 
-    getline(mon_graphe,ligne) ;      // Récupère la ligne avec le nombre de villes
-    nbr = atoi(ligne.c_str()) ;      // Récupère et converti le nombre de villes en int
-    Graphe mes_villes(nbr) ;
+    getline(mon_graphe, ligne);      // Recupere la ligne avec le nombre de villes
+    nbr = atoi(ligne.c_str());      // Recupere et convertit le nombre de villes en int
+    Graphe mes_villes(nbr);         // initialise un graphe de taille nbr
 
-    if(mon_graphe)
+    if (mon_graphe)
     {
-        while (getline(mon_graphe,ligne))       // Lecture de chaque ligne
+        while (getline(mon_graphe, ligne))       // Lecture de chaque ligne
         {
-            distance = atof(ligne.c_str()) ;        // Conversion du caractère en double
-            mes_villes.distances[i] = distance ;    // Initialisation du graphe
-            i++ ;
+            distance = atof(ligne.c_str());        // Conversion du caractère en double
+            mes_villes.distances[i] = distance;    // remplissage du graphe
+            i++;
         }
-        cout << mes_villes ;
-        mon_graphe.close() ;
+        cout << mes_villes;     //affichage du graphe
+        mon_graphe.close();
     }
     else
     {
-    cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
+        cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
     }
 
     ///////////////////////////////// ALGORITHME GENETIQUE ///////////////////////////////////////////////
-    
-    Chemin chemin_0(&mes_villes);
-    double proba = 0.3;
-    int iter = 50;
-    int q = 100;
-    int taille = 1000;
-    vector<Individu*> best;
-    int tours=10;
+
+    Chemin chemin_0(&mes_villes);   //initialisation du chemin par la methode du plus proche voisin
+
+        // PARAMETRES MODIFIABLES
+    int iter = 50;              //nombre d'iterations sans changement accepte
+    double proba = 0.8;         //probabilite de victoire dans tournoi
+    int q = 700;                //nombre de parents conserves dans elite
+    int taille = 1000;          //taille de la population
+    int tours=1;                //nombre de repetitions de l'algorithme
+
+
+    vector<Individu*> best;     //pour conserver les resultats de tous les tours
     best.resize(tours);
     Individu* result;
 
     for (int k = 0; k < tours; k++)
-        {result = algo_genetique(iter, proba, tournoi, elite, taille, &chemin_0, q);
-        best.at(k)=result->clone();}
+        {result = algo_genetique(iter, proba, tournoi, enfant, taille, &chemin_0, q);   //calcule le meilleur chemin possible
+        best.at(k)=result->clone();}    //range la solution dans le vecteur best
 
     double adaptation = 10000;
     vector<Individu*>::const_iterator it = best.begin();
 
     for (int k=0; k<tours; k++)
     {
+        //cout<<best.at(k)->adaptation()<<" "; //afficher les distances finales de chaque tour
         if (best.at(k)->adaptation() < adaptation)
         {
             adaptation = best.at(k)->adaptation();
             result = best.at(k)->clone();
         }
     }
+    cout<<endl;
 
-    result->print(cout);
+    result->print(cout);                        //afficher le chemin final
     cout << endl;
-    cout << result->adaptation() <<endl;
+    cout << result->adaptation() <<endl;        //afficher la longueur du chemin final
+
+    //cout << chemin_0.adaptation() <<endl;     //afficher la fonction d'adaptation du chemin initial
 
     ///////////////////////// ECRITURE D'UN FICHIER RENVOYANT LE CHEMIN OPTIMAL /////////////////////////
 
-    ofstream mon_chemin("E:/ENSTA/2A/SIM/202/Chemin.txt") ;
+    ofstream mon_chemin("C:/.../Chemin.txt");   //A modifier
 
-    Chemin& finalite = static_cast<Chemin&>(*result) ;
+    Chemin& finalite = static_cast<Chemin&>(*result);
 
     if (mon_chemin)
     {
-        mon_chemin << finalite ;
+        mon_chemin << finalite;
     }
     else
     {
         cout << "ERREUR: Impossible d'ouvrir le fichier en écriture." << endl;
     }
-    return 0 ;
+    return 0;
 }
