5513161be9ecf40eb30f22a4a524dcdf04018522
eliseharivel <60254184+eliseharivel@users.noreply.github.com>
2020-03-31 16:02:20 +0200

    commentaires

--- algoGenetique.h
+++ algoGenetique.h
@@ -5,60 +5,60 @@ using namespace std;
 #include <vector>
 #include <iostream>
 
-enum typeselection{roulette,rang,tournoi};
-enum typefinale{elite,enfant};
+enum typeselection { roulette, rang, tournoi };
+enum typefinale { elite, enfant };
 
-class Individu
+class Individu  //classe virtuelle
 {
-public :
+public:
     virtual ~Individu() {}; // destructeur virtuel
 
-    virtual double adaptation()=0; //fonction d'adaptation virtuelle pure a ecrire concretement dans la classe chemin
+    virtual double adaptation() = 0; //fonction d'adaptation virtuelle pure a ecrire concretement dans la classe chemin
 
-    virtual Individu& flip()=0 ;
+    virtual Individu& flip() = 0; //fonction de mutation virtuelle
 
-    virtual Individu* initialise_indiv()=0; // fonction d'initialisation d'individu virtuelle pure
-    
-    virtual Individu* clone()=0 ;   // fonction clone
-    
-    virtual pair<Individu*,Individu*> crossover(const Individu& ind_2)=0 ;
-    
-    virtual void print(ostream&) const =0;
+    virtual Individu* initialise_indiv() = 0; // fonction d'initialisation d'individu virtuelle pure
+
+    virtual Individu* clone() = 0;   // fonction de clonage virtuelle
+
+    virtual pair<Individu*, Individu*> crossover(const Individu& ind_2) = 0; //fonction d'hybridation virtuelle
+
+    virtual void print(ostream&) const = 0;
 };
 
 class Population
 {
-public :
+public:
     int taillePopulation;
     Individu* bestIndividu;
     vector<Individu*> popu;
 
-    Population(int p) {taillePopulation = p;popu.resize(p);bestIndividu=0;}; // constructeur par defaut
+    Population(int p) { taillePopulation = p; popu.resize(p); bestIndividu = 0; }; // constructeur par defaut
     Population(const Population& pop);
-    void clean() {for (int i=0 ; i<taillePopulation ; i++) delete popu[i] ;} ;
-    ~Population(){clean();}                              // destructeur
+    void clean() { for (int i = 0; i < taillePopulation; i++) delete popu[i]; };
+    ~Population() { clean(); }                              // destructeur
 
     // fonctions de selection des reproducteurs
 
     Population selection_roulette();
     Population selection_rang();
-    Population selection_tournoi(const double proba) ;
-    
+    Population selection_tournoi(const double proba);
+
     // operateur surcharge
-    
+
     Population& operator=(const Population& pop);
 };
 
-// fonctions de choix de population finale (mix entre parents et enfants)
+// fonctions de selection de population finale (mix entre parents et enfants)
 
-Population& pop_finale_enfants(const Population &parent, Population &enfant) ;
-Population pop_finale_elitisme(int q, const Population &parent, const Population &enfant);
+Population& pop_finale_enfants(const Population& parent, Population& enfant);
+Population pop_finale_elitisme(int q, const Population& parent, const Population& enfant);
 
-// fonctions auxiliaires generation de nombre aleatoire
+// fonctions auxiliaires : generation de nombre aleatoire
 
 int rand_0_n(int n); // fonction pour generer un entier aleatoire de 0 a (n-1)
 double reel_rand(double a, double b); // genere un nombre random entre a et b (reel)
 
-Individu* algo_genetique(int,double,typeselection,typefinale,int,Individu*,int);
+Individu* algo_genetique(int,double,typeselection,typefinale,int,Individu*,int); //fonction algorithme genetique
 
 #endif // ALGOGENETIQUE_H_INCLUDED
